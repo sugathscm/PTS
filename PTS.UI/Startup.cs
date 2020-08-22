d4ad55d2edb0d7340edc738f1d232f6bb45f301f
sugathscm <sugath.ch@gmail.com>
2020-08-22 19:42:48 +0530

    ADD

---
+++ PTS.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PTS.Startup))]
+namespace PTS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
